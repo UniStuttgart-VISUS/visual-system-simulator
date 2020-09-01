5eead0f39ab3ee5ee35bb57d6b76bd2e8ef9d1a5
Christoph Schulz <schulzcbs@gmail.com>
2020-09-01 14:42:58 +0200

    Speed up map generation.

--- vss/src/passes/retina/retina_map/macular_degeneration.rs
+++ vss/src/passes/retina/retina_map/macular_degeneration.rs
@@ -11,8 +11,8 @@ pub fn generate_simple(
     severity: u8,
 ) -> image::ImageBuffer<image::Rgba<u8>, Vec<u8>> {
     // convert severity from int between 0 and 100 to float between 0.5 and 1
-    let severity: f64 = severity as f64 / 100.0;
-    let severity: f64 = 1.0 - 0.5 * (1.0 - severity).powi(2);
+    let severity = severity as f64 / 100.0;
+    let severity = 1.0 - 0.5 * (1.0 - severity).powi(2);
     let radius = severity * res.0 as f64 / 3.0;
 
     generate(res, radius, severity)
@@ -25,30 +25,29 @@ pub fn generate_simple(
 ///
 /// - `res`    - resolution of the returned retina map
 /// - `radius` - radius of the affected area
-/// - `factor` - intensity of the effect, value between 0 and 1
+/// - `intensity` - intensity of the degeneration, value between 0.0 and 1.0
 ///
 pub fn generate(
     res: (u32, u32),
     radius: f64,
-    factor: f64,
+    intensity: f64,
 ) -> image::ImageBuffer<image::Rgba<u8>, Vec<u8>> {
-    let mut mapbuffer = image::ImageBuffer::new(res.0, res.1);
+    let mut map = image::ImageBuffer::new(res.0, res.1);
 
-    for (x, y, pixel) in mapbuffer.enumerate_pixels_mut() {
-        let distance_from_center = ((((res.0 / 2) as i32 - x as i32).pow(2)
-            + ((res.1 / 2) as i32 - y as i32).pow(2)) as f64)
-            .sqrt();
+    let cx = (res.0 / 2) as i32;
+    let cy = (res.1 / 2) as i32;
 
-        let x = 1.0 - (radius - distance_from_center).max(0.0) / radius;
-        //                        * 1.78 enlarges the black spot in the center
-        //                                                                   - 0.72 makes sure the outer areas are 0
-        let cells = 255
-            - (255.0 * (1.78 * factor * (-1.0 * x.powi(2)).exp() - 0.72))
-                .max(0.0)
-                .min(255.0) as u8;
-
+    for (x, y, pixel) in map.enumerate_pixels_mut() {
+        let distance_squared = (cx - x as i32).pow(2) + (cy - y as i32).pow(2);
+        // enlarges the black spot in the center.
+        let spot_factor = 1.78;
+        // ensures the outer areas are zero.
+        let tail_factor = 0.72;
+        let relative_falloff = 1.0 - (radius.powi(2) - distance_squared as f64).max(0.0) / radius.powi(2);
+        let x = spot_factor * (-relative_falloff).exp() - tail_factor;
+        let cells = 255 - (255.0 * x * intensity).max(0.0).min(255.0) as u8;
         *pixel = image::Rgba([cells, cells, cells, cells]);
     }
 
-    mapbuffer
+    map
 }
