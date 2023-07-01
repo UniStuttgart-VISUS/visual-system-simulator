f477d804df90de40d82d9855f24fbd298c843a09
Christoph Schulz <schulzcbs@gmail.com>
2023-07-01 18:44:33 +0200

    Tweak UI code a bit

--- vss-mobile/android/src/main/assets/js/simulator-bridge.js
+++ vss-mobile/android/src/main/assets/js/simulator-bridge.js
@@ -5,66 +5,95 @@ if (typeof Activity === "undefined") {
         querySettings: function () {
             let mockData = [{
                 "MockNodeA": {
-                    "MockParamA1": 42,
-                    "MockParamA2": false
+                    "MockParamA1": "A String",
+                    "MockParamA2": 42,
+                    "MockParamA3": true,
+                    "MockParamA4": "Another String"
                 }
             }, {
                 "MockNodeB": {
-                    "MockParamB1": "A String",
-                    "MockParamB2": true
+                    "MockParamB1": true,
+                    "MockParamB2": false,
+                    "MockParamB3": true,
+                    "MockParamB4": ""
                 }
             }];
             console.info("Querying Settings", mockData);
             return JSON.stringify(mockData);
         },
         postSettings: function (jsonString) {
-            assert(typeof jsonString === "string");
+            console.assert(typeof jsonString === "string");
             console.info("Posting Settings", JSON.parse(jsonString));
             return;
         }
     };
 }
 
+function buildAttributeInput(attributeId, attributeName, attributeValue, updateValue) {
+    var elAttribute;
+    if (typeof attributeValue == "boolean") {
+        elAttribute = $(`
+        <fieldset>
+            <label for="${attributeId}">
+                ${attributeName}<br>
+                <input type="checkbox" id="${attributeId}" name="${attributeId}" role="switch" ${attributeValue ? "checked" : ""}>
+            </label></fieldset>`);
+    } else if (typeof attributeValue == "number") {
+        elAttribute = $(`
+            <label for="${attributeId}">
+                ${attributeName}
+                <input type="number" id="${attributeId}" name="${attributeId}" value="${attributeValue}">
+            </label>
+            `);
+    } else {
+        elAttribute = $(`
+            <label for="${attributeId}">
+                ${attributeName}
+                <input type="text" id="${attributeId}" name="${attributeId}" value="${attributeValue}">
+            </label>`);
+    }
+    elAttribute.find('input').on("input", function () {
+        let type = $(this).attr('type');
+        var value;
+        if (type == "checkbox") {
+            value = $(this).prop('checked');
+        } else if (type == "number") {
+            value = parseFloat($(this).val());
+        } else {
+            value = $(this).val();
+        }
+        updateValue(value)
+    });
+    return elAttribute;
+}
+
 // Build DOM subtree with proper event listeners
 function buildSettingsPanel(settings) {
-    var elPanel = $(`<div class="settings-panel"></div>`);
+    let elPanel = $(`<div class="settings-panel"><form></form></div>`);
+    let elForm = elPanel.children('form');
     elPanel.settings = JSON.parse(JSON.stringify(settings));
-    for (var flowIndex = 0; flowIndex < settings.length; ++flowIndex) {
+    for (let flowIndex = 0; flowIndex < settings.length; ++flowIndex) {
         let flow = settings[flowIndex];
-        for (var nodeName in flow) {
-            elPanel.append(`<h6 id="sp__${nodeName}">${nodeName}</h6>`);
+        for (let nodeName in flow) {
+            let elAttributeSet = $(`
+                <fieldset>
+                    <legend><strong>${nodeName}</strong></legend>
+                </fieldset>`); 
             let node = flow[nodeName];
-            for (var attributeName in node) {
-                let attributeValue = node[attributeName];
+            for (let attributeName in node) {
                 let attributeId = `sp__${nodeName}__${attributeName}`;
-                let attributeType;
-                if (typeof attributeValue == "boolean") {
-                    attributeType = "checkbox";
-                } else if (typeof attributeValue == "number") {
-                    attributeType = "number";
-                } else {
-                    attributeType = "text";
-                }
-                let elLabel = $(`<label for="${attributeId}">${attributeName}</label>`)
-                let elInput = $(`<input type="${attributeType}" id="${attributeId}" name="${attributeId}"` +
-                    `placeholder="Value" value="${attributeValue}"></input>`)
-                elInput.on("input", function (flowIndex, nodeName, attributeName, attributeType) {
-                    return function () {
-                        var value;
-                        if (attributeType == "checkbox") {
-                            value = $(this).prop('checked');
-                        } else if (attributeType == "number") {
-                            value = parseFloat($(this).val());
-                        } else {
-                            value = $(this).val();
+                let attributeValue = node[attributeName];
+                let elAttribute = buildAttributeInput(
+                    attributeId, attributeName, attributeValue,
+                    function (flowIndex, nodeName, attributeName) {
+                        return function (value) {
+                            elPanel.settings[flowIndex][nodeName][attributeName] = value;
+                            Activity.postSettings(JSON.stringify(elPanel.settings));
                         }
-                        elPanel.settings[flowIndex][nodeName][attributeName] = value;
-                        Activity.postSettings(JSON.stringify(elPanel.settings));
-                    }
-                }(flowIndex, nodeName, attributeName, attributeType));
-                elLabel.append(elInput);
-                elPanel.append(elLabel);
+                    }(flowIndex, nodeName, attributeName));
+                elAttributeSet.append(elAttribute);
             }
+            elForm.append(elAttributeSet);
         }
     }
     return elPanel;
