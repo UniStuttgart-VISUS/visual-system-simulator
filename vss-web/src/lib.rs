b68e238364b7fc95fd0b5085fd53ae4aecad78fd
Christoph Schulz <schulzcbs@gmail.com>
2023-07-30 12:25:02 +0200

    Start working on web setup code

--- vss-web/src/lib.rs
+++ vss-web/src/lib.rs
@@ -2,7 +2,20 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn run() {
-    use web_sys::console;
 
-    console::log_1(&"Hello World!".into());
+    std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+    console_log::init().expect("could not initialize logger");
+    /* 
+    use winit::platform::web::WindowExtWebSys;
+    // On wasm, append the canvas to the document body
+    web_sys::window()
+        .and_then(|win| win.document())
+        .and_then(|doc| doc.body())
+        .and_then(|body| {
+            body.append_child(&web_sys::Element::from(window.canvas()))
+                .ok()
+        })
+        .expect("couldn't append canvas to document body");
+    wasm_bindgen_futures::spawn_local(run(event_loop, window));
+    */    
 }
