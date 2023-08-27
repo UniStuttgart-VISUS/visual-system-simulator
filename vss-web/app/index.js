a94d9b5d76ea05a6206699bb9a649e781f95d2d3
Christoph Schulz <schulzcbs@gmail.com>
2023-08-27 14:56:57 +0200

    Load a default image

--- vss-web/app/index.js
+++ vss-web/app/index.js
@@ -1,21 +1,22 @@
-import('./pkg').then(m => {
+import("./pkg").then(m => {
     let sim = m.Simulator.create_and_run("vss-container");
 
     let image = new Image();
     image.onload = () => {
-        let canvas = document.createElement('canvas');
-        let context = canvas.getContext('2d');
+        let canvas = document.createElement("canvas");
+        let context = canvas.getContext("2d");
         context.width = image.width;
         context.height = image.height;
         context.drawImage(image, 0, 0);
         let buffer = new Uint8Array(context.getImageData(0, 0, image.width, image.height).data.buffer);
         console.log("Posting", buffer, image.width, image.height);
         let res = sim.post_frame(buffer, image.width, image.height);
-        console.log(res);
+        console.log("Posting result:", res);
     };
+    image.src = "marketplace.png"
 
-    let imageUpload = document.getElementById('imageUpload');
-    imageUpload.addEventListener('change', (event) => {
+    let imageUpload = document.getElementById("imageUpload");
+    imageUpload.addEventListener("change", (event) => {
         const file = event.target.files[0];
         if (file) {
             const reader = new FileReader();
--- vss-web/app/webpack.config.js
+++ vss-web/app/webpack.config.js
@@ -1,24 +1,30 @@
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
 
 module.exports = {
-    entry: './index.js',
+    entry: "./index.js",
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'index.js',
+        path: path.resolve(__dirname, "dist"),
+        filename: "index.js",
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: 'index.html'
+            template: "index.html"
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                path.resolve(__dirname, "../../assets/marketplace.png")
+            ]
         }),
         new WasmPackPlugin({
             crateDirectory: path.resolve(__dirname, ".."),
             outDir: path.resolve(__dirname, "./pkg")
         })
     ],
-    mode: 'development',
+    mode: "development",
     experiments: {
         asyncWebAssembly: true
    }
